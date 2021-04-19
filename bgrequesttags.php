cd4f1ef20e8b3436426a66da4331e2f328907afc
jonah1957 <44168928+jonah1957@users.noreply.github.com>
2021-04-19 12:46:10 -0400

    Delete bgrequesttags.php

--- bgrequesttags.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Return a list of tags given the category.
-$field = $_POST["fields"];
-$tags = new stdClass();
-$tags->tags = array("canvas", "something", $field, "somethingelse");
-
-$resp = json_encode($tags);
-echo $resp;
- ?>
